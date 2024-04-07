ffde562f8900d8d5160c416c0358919b795d7fd7
harshit <harshitshrivastava340@gmail.com>
2024-04-07 23:40:44 +0530

    chatItem

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+
+
+const ChatItem =({content,role,}: {content: string;role: "user" | "assistant";}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChatItem
 
---
+++ frontend/src/pages/Chat.tsx
@@ -0,0 +1,123 @@
+import {Box, Avatar, Typography, Button, IconButton } from "@mui/material";
+import { useAuth } from "../context/AuthContext";
+import red from "@mui/material/colors/red";
+const Chat = () =>{
+  const auth = useAuth();
+    return (
+        <Box
+        sx={{
+          display: "flex",
+          flex: 1,
+          width: "100%",
+          height: "100%",
+          mt: 3,
+          gap: 3,
+        }}
+      >
+        <Box
+          sx={{
+            display: { md: "flex", xs: "none", sm: "none" },
+            flex: 0.2,
+            flexDirection: "column",
+          }}
+        >
+             <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17,29,39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+            <Avatar sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}>
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+            </Avatar>
+            <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+            You are talking to a ChatBOT
+          </Typography>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+            You can ask some questions related to Knowledge, Business, Advices,
+            Education, etc. But avoid sharing personal information
+          </Typography>
+          <Button
+           
+            sx={{
+              width: "200px",
+              my: "auto",
+              color: "white",
+              fontWeight: "700",
+              borderRadius: 3,
+              mx: "auto",
+              bgcolor: red[300],
+              ":hover": {
+                bgcolor: red.A400,
+              },
+            }}
+          >
+            Clear Conversation
+          </Button>
+        </Box>
+        </Box>
+        <Box
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, xs: 1, sm: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "40px",
+            color: "white",
+            mb: 2,
+            mx: "auto",
+            fontWeight: "600",
+          }}
+        >
+          Your Chat's
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60vh",
+            borderRadius: 3,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+            overflowX: "hidden",
+            overflowY: "auto",
+            scrollBehavior: "smooth",
+          }}
+        >
+         
+        </Box>
+        <div
+          style={{
+            width: "100%",
+            borderRadius: 8,
+            backgroundColor: "rgb(17,27,39)",
+            display: "flex",
+            margin: "auto",
+          }}
+        >
+        
+        </div>
+      </Box>
+
+        </Box>
+    )
+}
+
+export default Chat;
